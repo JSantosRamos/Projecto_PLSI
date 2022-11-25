74793314b9f3376f5568930f01388ae1fc01560b
JSantosRamos <jose.asantosramos@gmail.com>
2022-11-25 16:22:14 +0000

    Filtros carros
    Pertence ao commit anterior #31b2a448a5cc85a3d640bc06897320d1912d1924

--- common/models/VehicleSearch.php
+++ common/models/VehicleSearch.php
@@ -11,6 +11,15 @@ use common\models\Vehicle;
  */
 class VehicleSearch extends Vehicle
 {
+    public int $priceMin = 0;
+    public int $priceMax = 1000000;
+
+    public int $mileageMin = 0;
+    public int $mileageMax = 1000000;
+
+    //ublic $price_order_by;
+
+
     /**
      * {@inheritdoc}
      */
@@ -41,9 +50,9 @@ class VehicleSearch extends Vehicle
      */
     public function search($params, $frontendview = false)
     {
-        if($frontendview){
+        if ($frontendview) {
             $query = Vehicle::find()->where(['isActive' => 1]);
-        }else{
+        } else {
             $query = Vehicle::find();
         }
 
@@ -61,22 +70,32 @@ class VehicleSearch extends Vehicle
             return $dataProvider;
         }
 
+        //check price min and max values
+        $this->checkPriceMinAndMaxValuesPrice();
+
+        //check mileage filter values
+        $this->checkPriceMinAndMaxValuesMileage();
+
+        //check order price
+        // if ($this->price_order_by)
+
         // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
             'engine' => $this->engine,
             'year' => $this->year,
             'doorNumber' => $this->doorNumber,
-            'price' => $this->price,
             'isActive' => $this->isActive,
-        ]);
+        ])
+            ->andFilterWhere(['between', 'price', $this->priceMin, $this->priceMax])
+            ->andFilterWhere(['between', 'mileage', $this->mileageMin, $this->mileageMax]);
 
         $query->andFilterWhere(['like', 'brand', $this->brand])
             ->andFilterWhere(['like', 'model', $this->model])
             ->andFilterWhere(['like', 'serie', $this->serie])
             ->andFilterWhere(['like', 'type', $this->type])
             ->andFilterWhere(['like', 'fuel', $this->fuel])
-            ->andFilterWhere(['like', 'mileage', $this->mileage])
+            // ->andFilterWhere(['like', 'mileage', $this->mileage])
             ->andFilterWhere(['like', 'color', $this->color])
             ->andFilterWhere(['like', 'description', $this->description])
             ->andFilterWhere(['like', 'transmission', $this->transmission])
@@ -85,5 +104,63 @@ class VehicleSearch extends Vehicle
             ->andFilterWhere(['like', 'plate', $this->plate]);
 
         return $dataProvider;
+    }
+
+    /**
+     * @return void
+     */
+    public function checkPriceMinAndMaxValuesPrice(): void
+    {
+        if ($this->price == 1) {
+            $this->priceMin = 0;
+            $this->priceMax = 15000;
+        } elseif ($this->price == 2) {
+            $this->priceMin = 15000;
+            $this->priceMax = 30000;
+        } elseif ($this->price == 3) {
+            $this->priceMin = 30000;
+            $this->priceMax = 45000;
+        } elseif ($this->price == 4) {
+            $this->priceMin = 45000;
+            $this->priceMax = 60000;
+        } elseif ($this->price == 5) {
+            $this->priceMin = 60000;
+            $this->priceMax = 75000;
+        } elseif ($this->price == 6) {
+            $this->priceMin = 75000;
+            $this->priceMax = 90000;
+        } elseif ($this->price == 7) {
+            $this->priceMin = 90000;
+            $this->mileageMax = 1000000;
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function checkPriceMinAndMaxValuesMileage(): void
+    {
+        if ($this->mileage == 1) {
+            $this->mileageMin = 0;
+            $this->mileageMax = 25000;
+        } elseif ($this->mileage == 2) {
+            $this->mileageMin = 25000;
+            $this->mileageMax = 50000;
+        } elseif ($this->mileage == 3) {
+            $this->mileageMin = 50000;
+            $this->mileageMax = 75000;
+        } elseif ($this->mileage == 4) {
+            $this->mileageMin = 75000;
+            $this->mileageMax = 100000;
+        } elseif ($this->mileage == 5) {
+            $this->mileageMin = 100000;
+            $this->mileageMax = 125000;
+        } elseif ($this->mileage == 6) {
+            $this->mileageMin = 125000;
+            $this->mileageMax = 150000;
+        } elseif ($this->mileage == 7) {
+            $this->mileageMin = 150000;
+            $this->mileageMax = 1000000;
+        }
     }
 }
